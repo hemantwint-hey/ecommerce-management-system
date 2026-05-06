e65da79a76dcb858044902937dbf7866e92cee4e
hemantwint-hey <hemantwint@gmail.com>
2026-05-06 20:51:00 +0530

    Initial commit

--- src/main/java/org/example/ecommerce/repositories/CartRepository.java
+++ src/main/java/org/example/ecommerce/repositories/CartRepository.java
@@ -2,6 +2,9 @@ package org.example.ecommerce.repositories;
 
 import org.example.ecommerce.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart,Long> {
+    @Query("Select c from Cart c where c.user.email = ?1")
+    Cart findCartByEmail(String email);
 }
--- src/main/java/org/example/ecommerce/service/CartServiceImpl.java
+++ src/main/java/org/example/ecommerce/service/CartServiceImpl.java
@@ -1,12 +1,34 @@
 package org.example.ecommerce.service;
 
+import org.example.ecommerce.model.Cart;
 import org.example.ecommerce.payload.CartDTO;
+import org.example.ecommerce.repositories.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CartServiceImpl implements  CartService{
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    AuthUtil authUtil;
     @Override
     public CartDTO addProductToCart(Long productId, Integer quantity) {
+        // find the existing cart or create one
+        Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInEmail());
+        // retrieve product details
+        // perform validations
+        // create cart item
+        //save cart item
+        // return updated cart
         return null;
+    }
+    private Cart createCart(){
+         Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInEmail());
+         if(userCart != null)return userCart;
+         Cart cart = new Cart();
+         cart.setTotalPrice(0.00);
+         cart.setUser(authUtil.logge);
     }
 }
