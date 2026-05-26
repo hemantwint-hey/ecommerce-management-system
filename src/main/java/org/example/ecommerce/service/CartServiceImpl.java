87af5fb09a9bded757de6448635c258b5fc439e8
Aditya Nanda <adityasachida@gmail.com>
2026-05-26 21:01:17 +0530

    Initial commit

--- src/main/java/org/example/ecommerce/service/CartServiceImpl.java
+++ src/main/java/org/example/ecommerce/service/CartServiceImpl.java
@@ -1,9 +1,12 @@
 package org.example.ecommerce.service;
 
+import org.example.ecommerce.exceptions.APIException;
 import org.example.ecommerce.exceptions.ResourceNotFoundException;
 import org.example.ecommerce.model.Cart;
+import org.example.ecommerce.model.CartItem;
 import org.example.ecommerce.model.Product;
 import org.example.ecommerce.payload.CartDTO;
+import org.example.ecommerce.repositories.CartItemRepository;
 import org.example.ecommerce.repositories.CartRepository;
 import org.example.ecommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +20,14 @@ public class CartServiceImpl implements  CartService{
     @Autowired
     ProductRepository productRepository;
 
+
+
     @Autowired
     AuthUtil authUtil;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
     @Override
     public CartDTO addProductToCart(Long productId, Integer quantity) {
         // find the existing cart or create one
@@ -26,6 +35,13 @@ public class CartServiceImpl implements  CartService{
         // retrieve product details
         Product product = productRepository.findById(productId).orElseThrow(()-> new ResourceNotFoundException("Product", "productId", productId));
         // perform validations
+        CartItem cartItem = cartItemRepository.findCartItemByProductIdAndCartId(
+                cart.getCartId(),
+                productId
+        );
+        if(cartItem != null)throw new APIException("Product " + product.getProductName() + "already exists in the cart");
+
+        if( product.getQuantity() == 0)throw  new APIException(product.getProductName() + "is not available");
         // create cart item
         //save cart item
         // return updated cart
