8aa4ce2bb73520a3365cc99d1b1bb79d3bf54f31
hemantwint-hey <hemantwint@gmail.com>
2026-06-02 20:40:20 +0530

    added Fetching Users Cart functinonality

--- src/main/java/org/example/ecommerce/service/CartServiceImpl.java
+++ src/main/java/org/example/ecommerce/service/CartServiceImpl.java
@@ -101,7 +101,11 @@ public class CartServiceImpl implements  CartService{
         Cart cart = cartRepository.findCartByEmailAndCartId(emailID , cartId);
         if(cart == null)throw new ResourceNotFoundException("Cart", "cartId" , cartId);
         CartDTO cartDTO = modelMapper.map(cart,CartDTO.class);
-
+        cart.getCartItems().forEach(c -> c.getProduct().setQuantity(c.getQuantity()));
+        List<ProductDTO> products = cart.getCartItems().stream()
+                .map(p -> modelMapper.map(p.getProduct(), ProductDTO.class))
+                .toList();
+        cartDTO.setProducts(products);
         return cartDTO;
     }
 
